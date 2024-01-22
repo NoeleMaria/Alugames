f909197f5483b52d39cce455e3f303aec3d2750a
Noele Maria <noele.maria.nm@gmail.com>
2024-01-22 10:56:32 -0300

    Adicionando "confirmar" devolução

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-
+   
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');  
-        botao.textContent = 'Alugar';
+        let confirmar = prompt('Você tem certeza que deseja devolver o jogo? Digite "sim ou "não".');
+        if (confirmar = sim) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';}
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');  
